403075694e05f2b2b1eafb04bccc576d3271fd48
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-03 12:30:03 +0300

    changes

--- resources/views/livewire/account-profile.blade.php
+++ resources/views/livewire/account-profile.blade.php
@@ -45,7 +45,7 @@
                 <div class="w-full">
                     <h3 class="font-medium text-gray-900 text-left px-6">Account Settings</h3>
                     <div class="mt-5 w-full flex flex-col items-center overflow-hidden text-sm">
-                        <a href="#" class="w-full border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                        <a href="{{ route('profile') }}" class="w-full border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
                             <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
                             Change Password
                             <span class="text-gray-500 text-xs">>></span>

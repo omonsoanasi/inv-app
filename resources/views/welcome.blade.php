f1a6fa57956a6e123b89113003c087ddd20a819b
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-03 11:41:57 +0300

    chnages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,6 @@
             </div>
             <div
                 class="flex flex-col items-center justify-center w-20  h-20  bg-yellow-600 rounded-2xl text-yellow-600  shadow hover:shadow-md cursor-pointer mb-2 p-1 transition ease-in duration-300">
-                <i class="far fa-bus"></i>
                 <a href="{{ auth()->check() ? route('cust-withdraw') : route('login') }}">
                     <p class="text-sm font-extrabold text-white mt-1">Withdraw</p>
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-6 w-6 text-white" viewBox="0 0 16 16">
@@ -63,7 +62,15 @@
                     </svg>
                 </a>
             </div>
-
+            <div
+                class="flex flex-col items-center justify-center w-20  h-20  bg-amber-800 rounded-2xl text-amber-600  shadow hover:shadow-md cursor-pointer mb-2 p-1 transition ease-in duration-300">
+                <a href="{{ auth()->check() ? route('cust-withdraw') : route('login') }}">
+                    <p class="text-sm font-extrabold text-white mt-1">Application</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="h-6 w-6 text-white" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M8 0a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 4.095 0 5.555 0 7.318 0 9.366 1.708 11 3.781 11H7.5V5.5a.5.5 0 0 1 1 0V11h4.188C14.502 11 16 9.57 16 7.773c0-1.636-1.242-2.969-2.834-3.194C12.923 1.999 10.69 0 8 0m-.354 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V11h-1v3.293l-2.146-2.147a.5.5 0 0 0-.708.708z"/>
+                    </svg>
+                </a>
+            </div>
             <div
                 class="flex flex-col items-center justify-center w-20  h-20  bg-indigo-200  rounded-2xl  text-indigo-500 shadow hover:shadow-md cursor-pointer mb-2 p-1 bg-white transition ease-in duration-300">
                 <i class="far fa-mountains"></i>

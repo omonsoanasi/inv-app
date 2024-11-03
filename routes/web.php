015939a570994f5bb43aaacdd5bd1340f5d66d33
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-03 13:42:29 +0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use App\Livewire\SystemAdministration\Dashboard\Index;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->name('welcome');
 Route::view('/mobile', 'mobile');
 
 Route::view('dashboard', 'dashboard')

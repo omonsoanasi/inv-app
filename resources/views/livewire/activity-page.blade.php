9246cf630f8ddb2dd05b17b0bdcd3f1a5ae40e5b
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-14 13:45:24 +0300

    changes

--- resources/views/livewire/activity-page.blade.php
+++ resources/views/livewire/activity-page.blade.php
@@ -1,72 +1,44 @@
 <div>
-    <div class="overflow-hidden bg-gray-900">
+    <div class="overflow-hidden bg-green-900">
         <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div class="flex flex-col items-center justify-between xl:flex-row">
-                <div class="w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12">
+                <div class="w-full mb-12 xl:pr-16 xl:mb-0 ">
                     <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                        The quick, brown fox <br class="hidden md:block" />
-                        jumps over a <span class="text-teal-accent-400">lazy dog</span>
+                        <span class="text-teal-accent-400">PlanUSDT</span>
                     </h2>
-                    <p class="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudan, totam rem aperiam, eaque ipsa quae.
-                    </p>
-                    <a href="/" aria-label="" class="inline-flex items-center font-semibold tracking-wider transition-colors duration-200 text-teal-accent-400 hover:text-teal-accent-700">
-                        Learn more
-                        <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
-                            <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
+                    <div class="p-4 flex justify-center items-center flex-wrap">
+                      <span
+                          class="inline-flex items-center m-2 px-2 py-1 bg-gray-200 hover:bg-gray-300 rounded-full text-sm font-semibold text-gray-600">
+                        <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="w-4 h-4 fill-current" viewBox="0 0 16 16">
+                          <path fill-rule="evenodd" d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5zM3 3H2v1h1z"/>
+                          <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1z"/>
+                          <path fill-rule="evenodd" d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5zM2 7h1v1H2zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm1 .5H2v1h1z"/>
                         </svg>
-                    </a>
-                </div>
-                <div class="w-full max-w-xl xl:px-8 xl:w-5/12">
-                    <div class="relative">
-                        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute bottom-0 right-0 z-0 hidden w-32 -mb-8 -mr-20 text-teal-accent-400 lg:w-32 lg:-mr-16 sm:block">
-                            <defs>
-                                <pattern id="766323e1-e594-4ffd-a688-e7275079d540" x="0" y="0" width=".135" height=".30">
-                                    <circle cx="1" cy="1" r=".7"></circle>
-                                </pattern>
-                            </defs>
-                            <rect fill="url(#766323e1-e594-4ffd-a688-e7275079d540)" width="52" height="24"></rect>
+                        <span class="ml-1">
+                          Today's Tasks
+                        </span>
+                      </span>
+
+                        <span
+                            class="inline-flex items-center m-2 px-3 py-1 bg-green-200 hover:bg-green-300 rounded-full text-sm font-semibold text-green-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="h-4 w-4 fill-current" viewBox="0 0 16 16">
+                          <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9z"/>
+                          <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1zm1.038 3.018a6 6 0 0 1 .924 0 6 6 0 1 1-.924 0M0 3.5c0 .753.333 1.429.86 1.887A8.04 8.04 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5M13.5 1c-.753 0-1.429.333-1.887.86a8.04 8.04 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1"/>
                         </svg>
-                        <div class="relative bg-white rounded shadow-2xl p-7 sm:p-10">
-                            <h3 class="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
-                                Sign up for updates
-                            </h3>
-                            <form>
-                                <div class="mb-1 sm:mb-2">
-                                    <label for="name" class="inline-block mb-1 font-medium">Name</label>
-                                    <input
-                                        placeholder="John Doe"
-                                        required=""
-                                        type="text"
-                                        class="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                        id="name"
-                                        name="name"
-                                    />
-                                </div>
-                                <div class="mb-1 sm:mb-2">
-                                    <label for="email" class="inline-block mb-1 font-medium">E-mail</label>
-                                    <input
-                                        placeholder="john.doe@example.org"
-                                        required=""
-                                        type="text"
-                                        class="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-                                        id="email"
-                                        name="email"
-                                    />
-                                </div>
-                                <div class="mt-4 mb-2 sm:mb-4">
-                                    <button
-                                        type="submit"
-                                        class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                                    >
-                                        Subscribe
-                                    </button>
-                                </div>
-                                <p class="text-xs text-gray-600 sm:text-sm">
-                                    We respect your privacy. Unsubscribe at any time.
-                                </p>
-                            </form>
-                        </div>
+                        <span class="ml-1">
+                          Task resets in:
+                        </span>
+                      </span>
+
+                        <span
+                            class="inline-flex items-center m-2 px-3 py-1 bg-blue-200 hover:bg-blue-300 rounded-full text-sm font-semibold text-blue-600">
+                        <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z"
+                                                                                                                       fill="none"/><path
+                                d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/></svg>
+                        <span class="ml-1">
+                          Today's remaining tasks:
+                        </span>
+                      </span>
                     </div>
                 </div>
             </div>
@@ -74,21 +46,31 @@
     </div>
 
     <div class="grid grid-cols-3 gap-5">
-        <button class="tab-button text-white p-4 rounded bg-indigo-500 shadow-md flex items-center justify-center active" data-tab="in-progress">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
+        <button
+            class="tab-button text-white p-4 rounded bg-indigo-500 shadow-md flex items-center justify-center active"
+            data-tab="in-progress">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
             </svg>
             In Progress
         </button>
-        <button class="tab-button p-4 rounded bg-white text-indigo-500 shadow-md flex items-center justify-center" data-tab="completed">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
+        <button class="tab-button p-4 rounded bg-white text-indigo-500 shadow-md flex items-center justify-center"
+                data-tab="completed">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
             </svg>
             Completed
         </button>
-        <button class="tab-button p-4 rounded bg-white text-indigo-500 shadow-md flex items-center justify-center" data-tab="settings">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8h2a2 a=02 a=02 -00 -00 -00 -00 -00 -00 -00 -00"/>
+        <button class="tab-button p-4 rounded bg-white text-indigo-500 shadow-md flex items-center justify-center"
+                data-tab="settings">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" fill="none" viewBox="0 0 24 24"
+                 stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M17 8h2a2 a=02 a=02 -00 -00 -00 -00 -00 -00 -00 -00"/>
             </svg>
             Settings
         </button>
@@ -96,7 +78,8 @@
 
     <div class="shadow-xl border border-gray-100 font-light p-8 rounded text-gray-500 bg-white mt-6">
         <div id="in-progress" class="tab-content active">
-            Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse.
+            Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth
+            master cleanse.
         </div>
         <div id="completed" class="tab-content hidden">
             Mustache cliche tempor, williamsburg carles vegan helvetica.

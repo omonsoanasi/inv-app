a1c31dab7507ab458242a4b7aaf0dbd8de142b95
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-15 21:42:52 +0300

    changes

--- app/Livewire/ActivityPage.php
+++ app/Livewire/ActivityPage.php
@@ -3,22 +3,35 @@
 namespace App\Livewire;
 
 use App\Models\Activity;
+use App\Models\CompletedTask;
 use Livewire\Component;
 
 class ActivityPage extends Component
 {
     public $id;
     public $activity;
+    public $user_id;
 
     public function mount($id)
     {
         $this->id = $id;
         $this->activity = Activity::find($id);
+        $this->user_id = auth()->user()->id;
+
     }
+
     public function completeTask()
     {
-        dd($this->activity);
+        // Create completed task
+        CompletedTask::create([
+            'activity_id' => $this->activity->id,
+            'user_id' => $this->user_id,
+        ]);
+
+        session()->flash('message', 'Task has been completed');
+        return redirect()->route('activity-page', $this->activity->id);
     }
+
     public function render()
     {
         return view('livewire.activity-page')->layout('layouts.app');

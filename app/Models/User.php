50f7071fdefc22337337456b05c8c466be59b90b
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-16 15:19:34 +0300

    changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;

0f82def757de9f1a8f0d779de719cfaa65434a7c
Emmanuel Anasi <eanasi@strathmore.edu>
2024-11-29 00:47:27 +0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -43,10 +43,11 @@ Route::get('/forgot-password', function () {
 })->middleware('guest')->name('password.request');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/app-index', Index::class)->name('app-index');
-    Route::get('/app-dashboard', AdminDashboard::class)->name('app-dashboard');
-    Route::get('/access-control', AccessControl::class)->name('access-control');
-    Route::get('/cms', Cms::class)->name('cms');
+    Route::get('/app-index', Index::class)->name('app-index')->middleware('role:System Administrator');
+    Route::get('/app-dashboard', AdminDashboard::class)->name('app-dashboard')->middleware('role:System Administrator');
+    Route::get('/access-control', AccessControl::class)->name('access-control')->middleware('role:System Administrator');
+    Route::get('/cms', Cms::class)->name('cms')->middleware('role:System Administrator');
+
     Route::get('cust-recharge', CustRecharge::class)->name('cust-recharge');
     Route::get('/trc20-usdt', Trc20Usdt::class)->name('trc20-usdt');
     Route::get('/bep20-usdt', Bep20Usdt::class)->name('bep20-usdt');
@@ -55,17 +56,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/account-profile', AccountProfile::class)->name('account-profile');
     Route::get('/cust-team', CustTeam::class)->name('cust-team');
 
-    Route::get('/deposit-index', DepositIndex::class)->name('deposit-index');
-    Route::get('/system-configs', SystemConfigs::class)->name('system-configs');
-    Route::get('/Trc20usdt-instructions', Trc20Usdt::class)->name('Trc20usdt-instructions');
+    Route::get('/deposit-index', DepositIndex::class)->name('deposit-index')->middleware('role:System Administrator');
+    Route::get('/system-configs', SystemConfigs::class)->name('system-configs')->middleware('role:System Administrator');
+    Route::get('/Trc20usdt-instructions', Trc20Usdt::class)->name('Trc20usdt-instructions')->middleware('role:System Administrator');
 
-    Route::get('confirm-trc20-payment/{id}', ConfirmTrc20Payment::class)->name('confirm-trc20-payment');
-    Route::get('confirm-bep20-payment/{id}', ConfirmBep20Payment::class)->name('confirm-bep20-payment');
-    Route::get('confirm-bnb-payment/{id}', ConfirmBnbPayment::class)->name('confirm-bnb-payment');
+    Route::get('confirm-trc20-payment/{id}', ConfirmTrc20Payment::class)->name('confirm-trc20-payment')->middleware('role:System Administrator');
+    Route::get('confirm-bep20-payment/{id}', ConfirmBep20Payment::class)->name('confirm-bep20-payment')->middleware('role:System Administrator');
+    Route::get('confirm-bnb-payment/{id}', ConfirmBnbPayment::class)->name('confirm-bnb-payment')->middleware('role:System Administrator');
 
-    Route::get('customer-withdrawals', CustomerWithdrawals::class)->name('customer-withdrawals');
-    Route::get('complete-withdrawal/{id}', CompleteWithdrawal::class)->name('complete-withdrawal');
-    Route::get('/chat-response/{id}', ChatResponse::class)->name('chat-response');
+    Route::get('customer-withdrawals', CustomerWithdrawals::class)->name('customer-withdrawals')->middleware('role:System Administrator');
+    Route::get('complete-withdrawal/{id}', CompleteWithdrawal::class)->name('complete-withdrawal')->middleware('role:System Administrator');
+    Route::get('/chat-response/{id}', ChatResponse::class)->name('chat-response')->middleware('role:System Administrator');
 
     Route::get('activity-page/{id}', ActivityPage::class)->name('activity-page');
     Route::get('/pro-activities', ProActivities::class)->name('pro-activities');
